b99f48776541aac5fb4dda45a543373ce8468a63
artserjulietta <110678368+artserjulietta@users.noreply.github.com>
2023-10-15 13:15:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import telebot 
 from config import token
 
-from logic import Pocemon
+from logic import Pokemon
 
 bot = telebot.TeleBot(token) 
 
 @bot.message_handler(commands=['go'])
 def start(message):
-    if message.from_user.username not in Pocemon.pocemons.keys():
-        pocemon = Pocemon(message.from_user.username)
-        bot.send_message(message.chat.id, pocemon.info())
-        bot.send_photo(message.chat.id, pocemon.show_img())
+    if message.from_user.username not in Pokemon.pokemons.keys():
+        pokemon = Pokemon(message.from_user.username)
+        bot.send_message(message.chat.id, pokemon.info())
+        bot.send_photo(message.chat.id, pokemon.show_img())
     else:
         bot.reply_to(message, "Ты уже создал себе покемона")
 
