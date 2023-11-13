0e9e7a5fe5f6e383dfc2e7f2a3b1fd0412481366
artserjulietta <110678368+artserjulietta@users.noreply.github.com>
2023-11-13 17:40:32 +0300

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,24 +1,33 @@
 from random import randint
-
 import requests
-
-# Функция для получения рандомной картинки покемона через API
-def get_img():
-    url = f'https://pokeapi.co/api/v2/pokemon/{randint(1,1000)}'
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        return (data['sprites']['other']['home']['front_default'])
-    else:
-        get_img()
 
 class Pokemon:
     pokemons = {}
     # Инициализация объекта (конструктор)
-    def __init__(self, name):
-        self.name = name    # Поле или атрибут класса
-        self.img = get_img()
-        Pokemon.pokemons[name] = self
+    def __init__(self, pokemon_trainer):
+
+        self.pokemon_trainer = pokemon_trainer   
+
+        self.pokemon_number = randint(1,1000)
+        self.img = self.get_img()
+        self.name = self.get_name()
+
+        Pokemon.pokemons[pokemon_trainer] = self
+
+    # Метод для получения картинки покемона через API
+    def get_img(self):
+        pass
+    
+    # Метод для получения имени покемона через API
+    def get_name(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['forms'][0]['name'])
+        else:
+            return "Pikachu"
+
 
     # Метод класса для получения информации
     def info(self):
@@ -27,9 +36,6 @@ class Pokemon:
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
-
-
-
 
 
 
